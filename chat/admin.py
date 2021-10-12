44b1f7c3068fe3c6e085f09fcee826a6a9518047
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-12 18:01:21 +0800

    Added extended admin view for RoomUser model

--- chat/admin.py
+++ chat/admin.py
@@ -3,13 +3,22 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Room)
-admin.site.register(RoomUser)
+# admin.site.register(RoomUser)
 
 @admin.register(Message)
 class Message(admin.ModelAdmin):
     list_display = ('id', 'username', 'room', 'date_added',
                     'content')
     list_filter = ('username', 'room__name', 'date_added')
+    ordering = ("id",)
+
+    def get_name(self, obj):
+        return obj.user
+
+@admin.register(RoomUser)
+class Message(admin.ModelAdmin):
+    list_display = ('id', 'room', 'username')
+    list_filter = ('username', 'room__name')
     ordering = ("id",)
 
     def get_name(self, obj):
