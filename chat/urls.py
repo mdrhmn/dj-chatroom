ac997d539b20831f14885addad51900713870a36
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-14 04:20:36 +0800

    Changed URL paths for index and room and added paths for Django generic auth views

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('home/', views.index, name='index'),
+    path('chat/<str:room_name>/', views.room, name='room'),
     path('check_room', views.check_room, name='check_room'),
+
+    # Django default authentication generic views
+    path('', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
