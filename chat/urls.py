5ae824f9831ac94321910acdff6f58e76acfb697
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-14 04:40:48 +0800

    Added redirect_authenticated_user=True param in generic auth view so that logged in users will auto redirect to home if they go to login URL path

--- chat/urls.py
+++ chat/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('check_room', views.check_room, name='check_room'),
 
     # Django default authentication generic views
-    path('', auth_views.LoginView.as_view(), name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
