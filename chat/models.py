0def7649b15303b27660db96d95aba47d2172dd8
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-14 04:24:14 +0800

    Revamped all custom models to link with default User model instead of creating custom username field, added new Profile model and added signals-decorated function for creating new Profile instance after new User instance is created

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,8 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.conf import settings
 from django.db import models
-
 
 class Room(models.Model):
     name = models.CharField(max_length=255)
@@ -9,15 +12,22 @@ class Room(models.Model):
 
 
 class RoomUser(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    username = models.CharField(max_length=255)
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name='room_user')
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # username = models.CharField(max_length=255)
+    status = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
 
 class Message(models.Model):
-    username = models.CharField(max_length=255)
+    # username = models.CharField(max_length=255)
+    # Use ForeignKey instead of OneToOneField due to one-to-many relationship between User and Message
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='message_user')
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     content = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
@@ -27,3 +37,24 @@ class Message(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+# We will now define signals so our Profile model will be automatically created/updated 
+# when we create/update User instances with default settings.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
