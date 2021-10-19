4159a6da0ad04a3d53c8fd1e8df199b231307da2
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-19 16:31:15 +0800

    Updated asgi.py

--- django_chat_channels_redis/asgi.py
+++ django_chat_channels_redis/asgi.py
@@ -18,13 +18,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import chat.routing
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
-})
+application = get_default_application()
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     )
+# })
