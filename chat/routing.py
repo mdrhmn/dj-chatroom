bd8784457f482cc842f4f1872f37d812fc59537e
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-19 17:14:27 +0800

    Replaced ws/ to wss/ to support web socket connection over HTTPS

--- chat/routing.py
+++ chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
-    path('ws/home/', consumers.LoginStatusConsumer.as_asgi()),
+    path('wss/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('wss/home/', consumers.LoginStatusConsumer.as_asgi()),
 ]
